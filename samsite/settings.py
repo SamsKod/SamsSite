0cb0ee3e908875ecaac79b6633c4e72f2244eac2
samuelwigren <>
2023-04-15 07:24:35 +0000

    add new ALLOWED_HOSTS to setting.py

--- samsite/settings.py
+++ samsite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["samsite.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["samsite.herokuapp.com", "8000-samskod-samssite-pof8udnnvj.us2.codeanyapp.com", "localhost"]
 
 
 # Application definition
