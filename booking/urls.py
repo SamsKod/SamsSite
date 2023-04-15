93e4f84a0bde464ead101ea88eb3a93b382fe07e
samuelwigren <>
2023-04-15 15:12:07 +0000

    Add booking post field and view

--- booking/urls.py
+++ booking/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='booking-home'),
+    path('', views.booking, name='booking-home'),
     path('about/', views.about, name='booking-about'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
 
-def home(request):
-    return render(request, 'booking/home.html', {'title': 'Bookings'})
+
+def booking(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'booking/home.html', context)
 
 
 def about(request):
